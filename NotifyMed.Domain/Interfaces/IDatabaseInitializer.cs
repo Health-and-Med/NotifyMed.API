580a704aba576d27e61bad6e39cb37574596123a
GM-MP-2023 <mpdevs@outlook.com>
2025-02-02 20:09:39 -0300

    Ajustado

---
+++ NotifyMed.Domain/Interfaces/IDatabaseInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyMed.Domain.Interfaces
+{
+    public interface IDatabaseInitializer
+    {
+        void Initialize();
+    }
+}
+
---
+++ NotifyMed.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using NotifyMed.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyMed.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUsernameAsync(string email);
+        Task AddUserAsync(User user);
+    }
+}
+
